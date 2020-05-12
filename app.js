1084c5ddab97b99378aa371928c1f965986dd05f
Raman Sharma <56464662+RamanSharma100@users.noreply.github.com>
2020-05-12 16:35:54 +0530

    updated home page

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.set('view engine', 'ejs');
 //setup static
 app.use(express.static(__dirname + '/assests'));
 
+//global variables
+app.use((req, res, next) => {
+  res.locals.currentUrl =
+    req.protocol + '://' + req.get('host') + req.originalUrl;
+  next();
+});
 //pages
 app.get('/', (req, res) => {
   res.render('index');
