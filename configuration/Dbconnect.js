d539fd5a1cd113fd4f59f9b96bb65ded84d995eb
Raman Sharma <56464662+RamanSharma100@users.noreply.github.com>
2020-05-18 16:21:34 +0530

    database

--- configuration/Dbconnect.js
+++ configuration/Dbconnect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-// const url =
-//   'mongodb+srv://ramansharma:Krantivir100@cluster0-hicpo.mongodb.net/my_portfolio?retryWrites=true&w=majority';
+
 const url = 'mongodb://localhost:27017/my_portfolio';
 
 const connectDB = async () => {
